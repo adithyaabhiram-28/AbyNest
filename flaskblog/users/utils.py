26ea434c6697ae46fdfd41811741969ba8fffe72
adithyaabhiram-28 <adithya.abhiram.2805@gmail.com>
2026-05-05 14:35:58 +0530

    Final

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -2,7 +2,7 @@ import cloudinary.uploader
 import os
 import secrets
 from PIL import Image
-from flask import url_for, current_app
+from flask import url_for, current_app, flash
 from flask_mail import Message
 from flaskblog import mail
 
